42167dc544bcb606b38dc2a957dd9d958778ffc2
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-05-11 20:49:06 +0545

    Initial Commit

---
+++ SwiftUIMovieApp/SwiftUIMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMovieAppApp.swift
+//  SwiftUIMovieApp
+//
+//  Created by Saujan Bindukar on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
